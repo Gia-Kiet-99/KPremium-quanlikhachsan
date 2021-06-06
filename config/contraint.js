6b760e49f9fe90547a5c1ae663ccaaf030b3d662
kptankhoa <kptankhoa@gmail.com>
2021-06-06 16:46:35 +0700

    add a bunch of schemas

---
+++ config/contraint.js
@@ -0,0 +1,18 @@
+"use strict";
+module.exports = {
+    USER_TYPE: {
+        STAFF: "STAFF",
+        MANAGER: "MANAGER"
+    },
+
+    CUSTOMER_TYPE: {
+        DC: "DC",
+        FC: "FC"
+    },
+
+    ROOM_TYPE: {
+        A: "A",
+        B: "B",
+        C: "C"
+    }
+}
