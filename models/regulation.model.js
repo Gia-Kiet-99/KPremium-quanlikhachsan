8d52f69494c90e058009ddde5c2335e444f6dd43
kptankhoa <kptankhoa@gmail.com>
2021-06-25 22:28:18 +0700

    xong thay doi quy dinh

--- models/regulation.model.js
+++ models/regulation.model.js
@@ -23,8 +23,34 @@ const changeRoomSurchargeRate = async (typeId, surchargeRate) => {
 		throw Error(e);
 	}
 }
+
+const changeCustomerSurchargeRate = async (typeId, surchargeRate) => {
+	try {
+		const res = await knex('customer_type')
+			.update({surcharge: surchargeRate})
+			.where('type_id', typeId);
+		return res;
+	} catch (e) {
+		console.log(e);
+		throw Error(e);
+	}
+}
+
+const changeRoomRate=async (typeId, newRoomRate) =>{
+	try {
+		const res = await knex('room_type')
+			.update({room_rate: newRoomRate})
+			.where('type_id', typeId);
+		return res;
+	} catch (e) {
+		console.log(e);
+		throw Error(e);
+	}
+}
 module.exports = {
 	changeMaxGuestsOfRoom,
-	changeRoomSurchargeRate
+	changeRoomSurchargeRate,
+	changeRoomRate,
+	changeCustomerSurchargeRate
 }
 
--- routes/regulation.route.js
+++ routes/regulation.route.js
@@ -3,5 +3,7 @@ const regulationController = require('../controllers/regulation.controller');
 
 router.post('/room-type/:typeId/surcharge-rate', regulationController.changeSurchargeRate);
 router.post('/room-type/:typeId/max-guest', regulationController.changeMaxGuest);
+router.post('/room-type/:typeId/room-rate', regulationController.changeRoomRate);
+router.post('/customer-type/:typeId/surcharge', regulationController.changeCustomerSurchargeRate);
 
 module.exports = router;
